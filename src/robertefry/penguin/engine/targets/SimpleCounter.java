97f17bafb5e7179073c8ed7c4aa72fa91a034541
Robert E Fry <robert.e.fry@protonmail.com>
2019-01-29 01:56:01 +0000

    SimpleCounter

---
+++ src/robertefry/penguin/engine/targets/SimpleCounter.java
@@ -0,0 +1,24 @@
+package robertefry.penguin.engine.targets;
+
+import robertefry.penguin.engine.Engine;
+import robertefry.penguin.engine.target.Targetable;
+
+public class SimpleCounter implements Targetable {
+	
+	private long count = 0;
+	
+	@Override
+	public void tick(Engine engine) {
+		Targetable.super.tick(engine);
+		count++;
+	}
+	
+	public long getCount() {
+		return count;
+	}
+	
+	public void reset() {
+		count = 0;
+	}
+
+}
