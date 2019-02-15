b83d2ddee1bc3c832dc7339505f42d4552d9532a
Robert E Fry <robert.e.fry@protonmail.com>
2019-02-15 19:00:02 +0000

    helper classes

---
+++ src/robertefry/penguin/input/mouse/listener/MouseObjectAdapter.java
@@ -0,0 +1,45 @@
+
+package robertefry.penguin.input.mouse.listener;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseWheelEvent;
+
+/**
+ * @author Robert E Fry
+ * @date 15 Feb 2019
+ */
+public class MouseObjectAdapter implements MouseObjectListener {
+
+	@Override
+	public void onButtonPress( MouseEvent e ) {
+	}
+
+	@Override
+	public void onButtonRelease( MouseEvent e ) {
+	}
+
+	@Override
+	public void onButtonClick( MouseEvent e ) {
+	}
+
+	@Override
+	public void onMouseEnter( MouseEvent e ) {
+	}
+
+	@Override
+	public void onMouseExit( MouseEvent e ) {
+	}
+
+	@Override
+	public void onMouseMove( MouseEvent e ) {
+	}
+
+	@Override
+	public void onMouseDrag( MouseEvent e ) {
+	}
+
+	@Override
+	public void onWheelAction( MouseWheelEvent e ) {
+	}
+
+}
---
+++ src/robertefry/penguin/input/mouse/listener/MouseObjectListener.java
@@ -0,0 +1,9 @@
+
+package robertefry.penguin.input.mouse.listener;
+/**
+ * @author Robert E Fry
+ * @date 15 Feb 2019
+ */
+public interface MouseObjectListener extends MouseButtonListener, MouseActionListener {
+
+}
